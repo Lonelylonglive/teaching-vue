8d6ca2f497fe5188a8050995ef541ac3a61d5ec6
shenglong.li <913937000@qq.com>
2017-12-20 09:35:34 +0800

    change

---
+++ src/page/detail/detail.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="wrapper">
+      <img class="img" src="http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_140x140_80f63803.jpg">
+      <p class="text">this is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail pagethis is detail page</p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped lang="stylus">
+  @import '../../assets/stylus/varibles.styl'
+  .wrapper
+    display: flex
+    .img
+      width: 1.6rem
+      height: 1.6rem
+    .text
+      flex: 1
+      background: $backgroundColor
+</style>
